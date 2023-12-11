3a889dcce962a14bf7c0eafe40b6a66bebc9c56d
butt3rs <tekrimon4ever@yandex.ru>
2023-12-12 01:21:54 +0500

    Добавлены элементы управления

--- Calendar/View/CalendarDateRow.swift
+++ Calendar/View/CalendarDateRow.swift
@@ -13,26 +13,122 @@ struct CalendarDateRow: View {
     @State var day: Int = 0
     var stringDate: String {get{String(day)}}
     @ObservedObject var CalendVM: CalendarVM = CalendarVM()
-    
+    @State var selectedYear: String = "2023"
     init() {
-        print(Date().startOfMonth.description(with: .current))
         formater.dateFormat = "dd-MM-yyyy"
-        print(Calendar.current.component(.weekday, from: formater.date(from: "04-12-2023")!))
-        print(Calendar.current.component(.weekday, from: formater.date(from: "05-12-2023")!))
-        print(Calendar.current.component(.weekday, from: formater.date(from: "06-12-2023")!))
-        print(Calendar.current.component(.weekday, from: formater.date(from: "07-12-2023")!))
-        print(Calendar.current.component(.weekday, from: formater.date(from: "08-12-2023")!))
-        print(Calendar.current.component(.weekday, from: formater.date(from: "09-12-2023")!))
-        print(Calendar.current.component(.weekday, from: formater.date(from: "10-12-2023")!))
-//        print(CalendVM.days)
     }
     
     var body: some View {
-        Text(String(CalendVM.startDayOfWeek))
-        Text(String(CalendVM.currentYear))
-        Text(String(CalendVM.currentMonth))
-        Button(action: {CalendVM.nextMonth()}, label: {Text("Next")})
-        Button(action: {CalendVM.previousMonth()}, label: {Text("Previous")})
+        Button(action: {CalendVM.goBack()}, label:{
+            ZStack{
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.white)
+                    .shadow(radius: 3)
+                    .frame(width: 370, height: 40)
+                HStack(){
+                    Image(systemName: "arrowshape.forward.circle")
+                        .rotationEffect(.degrees(180))
+                    Text("Go back")
+                        .font(.custom("Roboto Bold", size: 18))
+                        .padding(.horizontal)
+                    Image(systemName: "arrowshape.forward.circle")
+                        .rotationEffect(.degrees(180))
+                }
+                .frame(minWidth: 180, minHeight: 40)
+            }
+        })
+        Picker("Год", selection: $selectedYear){
+            ForEach(1...2100, id: \.description){ i in
+                Text(String(i))
+            }
+        }
+        .onChange(of: selectedYear) {
+            CalendVM.setYear(year: Int(selectedYear)!)
+        }
+        Text(String("Я календарь переверну, и снова..."))
+            .fontWeight(.bold)
+        HStack{
+            Button(action: {CalendVM.previousYear()}, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.white)
+                        .shadow(radius: 3)
+                        .frame(width: 180, height: 40)
+                    HStack(){
+                        Image(systemName: "arrowshape.forward.circle")
+                            .rotationEffect(.degrees(180))
+                        Text(String(CalendVM.currentYear - 1))
+                            .font(.custom("Roboto Bold", size: 18))
+                            .padding(.leading)
+                    }
+                    .frame(minWidth: 180, minHeight: 40)
+                }
+            })
+            Button(action: {CalendVM.nextYear()}, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.white)
+                        .shadow(radius: 3)
+                        .frame(width: 180, height: 40)
+                    HStack(){
+                        Text(String(CalendVM.currentYear + 1))
+                            .font(.custom("Roboto Bold", size: 18))
+                            .padding(.trailing)
+                        Image(systemName: "arrowshape.forward.circle")
+                    }
+                    .frame(minWidth: 180, minHeight: 40)
+                }
+            })
+        }
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.white)
+                .shadow(radius: 3)
+                .frame(width: 370, height: 40)
+            Text(String(CalendVM.currentYear))
+                .font(.custom("Roboto Bold", size: 24))
+        }
+        HStack{
+            Button(action: {CalendVM.previousMonth()}, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.white)
+                        .shadow(radius: 3)
+                        .frame(width: 180, height: 40)
+                    HStack(){
+                        Image(systemName: "arrowshape.forward.circle")
+                            .rotationEffect(.degrees(180))
+                        Text(CalendarVM.months[CalendVM.getMonth(much: -1)])
+                            .font(.custom("Roboto Bold", size: 18))
+                            .padding(.leading)
+                    }
+                    .frame(minWidth: 180, minHeight: 40)
+                }
+            })
+            Button(action: {CalendVM.nextMonth()}, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.white)
+                        .shadow(radius: 3)
+                        .frame(width: 180, height: 40)
+                    HStack(){
+                        Text(CalendarVM.months[CalendVM.getMonth(much: 1)])
+                            .font(.custom("Roboto Bold", size: 18))
+                            .padding(.trailing)
+                        Image(systemName: "arrowshape.forward.circle")
+                    }
+                    .frame(minWidth: 180, minHeight: 40)
+                }
+            })
+        }
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.white)
+                .shadow(radius: 3)
+                .frame(width: 370, height: 40)
+            Text(CalendarVM.months[CalendVM.currentMonth])
+                .font(.custom("Roboto Bold", size: 24))
+        }
         HStack(spacing: 32){
             Text("Пн")
             Text("Вт")
@@ -46,8 +142,8 @@ struct CalendarDateRow: View {
             VStack{
                 ForEach(CalendVM.days, id: \.hashValue){ days in
                     HStack{
-                        ForEach(days, id: \.hashValue){ i in
-                            CalendarDate(isActive: true, calendarDate: formater.date(from: i)!)
+                        ForEach(days, id: \.id){ i in
+                            CalendarDate(isActive: i.activeDay, calendarDate: formater.date(from: i.dayString)!)
                         }
                     }
                 }
